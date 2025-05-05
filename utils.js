5a6f762235439ffb3df0bd77eb3b0f1c3b2fe2e3
GuyLeDouce <155856411+GuyLeDouce@users.noreply.github.com>
2025-05-05 08:34:08 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,8 @@
+// utils.js
+function getNFTImageURL(tokenId) {
+  return `https://ipfs.io/ipfs/bafybeie5o7afc4yxyv3xx4jhfjzqugjwl25wuauwn3554jrp26mlcmprhe/${tokenId}.jpg`;
+}
 
+module.exports = {
+  getNFTImageURL,
+};
